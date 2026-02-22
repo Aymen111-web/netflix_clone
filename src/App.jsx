aa98f82f908d32894da291810e78dbe0665ba0f0
Aymen Mohammed <aymimanmoh8@gmail.com>
2026-02-23 00:39:17 +0300

    the rows are rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './App.css'
 import Row from "./Row";
+import requests from "./requests";
+
 
 function App() {
 
@@ -9,10 +11,23 @@ function App() {
 
     <div className="App">
       <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="TRENDING NOW" />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   )
 }
 
-export default App
+export default App;
